8478a4ba01f29ae8d1d70def5c4c56851baaa0fa
JohnMoran <johnnymoran44@hotmail.com>
2021-11-15 13:50:46 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [results, setResults] = useState([]);
+	useEffect(() => {
+		getResults();
+	}, []);
+
+	const getResults = async () => {
+		const res = await axios.get("http://hn.algolia.com/api/v1/search?query=reacthooks");
+		console.log(res);
+		setResults(res.data.hits);
+	};
+
+	return (
+		<div>
+			<ul>
+				{results.map((item) => {
+					return (
+						<li key={item.objectID}>
+							<a href={item.url}>{item.title}</a>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
-
-export default App;
