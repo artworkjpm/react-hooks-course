c1b3e03a5a3064f3a43472a1d60e39df0a5ba245
JohnM <johnnymoran44@hotmail.com>
2021-11-16 09:56:40 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,74 +1,32 @@
-import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
+import CrudApp from "./pages/CrudApp";
+import NewsPage from "./pages/NewsPage";
 
 export default function App() {
-	const [results, setResults] = useState([]);
-	const [query, setQuery] = useState("react hooks");
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
-	const searchInputRef = useRef();
-
-	useEffect(() => {
-		getResults();
-		// eslint-disable-next-line
-	}, []);
-
-	const getResults = async () => {
-		setLoading(true);
-		try {
-			const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`);
-			setResults(response.data.hits);
-		} catch (err) {
-			setError(err);
-		}
-		setLoading(false);
-	};
-
-	const handleSearch = (event) => {
-		event.preventDefault();
-		getResults();
-	};
-
-	const handleClearSearch = () => {
-		setQuery("");
-		searchInputRef.current.focus();
-	};
-
 	return (
-		<div className="container max-w-md mx-auto p-4 m-2 bg-purple-100 shadow-lg rounded">
-			<img src="react.png" alt="React Logo" className="float-right h-12" />
-			<h1 className="text-grey-darkest font-thin">Hooks News</h1>
-			<form onSubmit={handleSearch} className="mb-2">
-				<input
-					type="text"
-					onChange={(event) => setQuery(event.target.value)}
-					value={query}
-					ref={searchInputRef}
-					className="border p-1 rounded"
-				/>
-				<button type="submit" className="bg-yellow-500 hover:bg-red-500 rounded m-1 p-1">
-					Search
-				</button>
-				<button type="button" onClick={handleClearSearch} className="bg-green-300 text-white p-1 rounded">
-					Clear
-				</button>
-			</form>
+		<BrowserRouter>
+			<nav className="font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between py-4 px-6 bg-white shadow sm:items-baseline w-full">
+				<div className="mb-2 sm:mb-0">
+					<Link className="text-2xl no-underline text-grey-darkest hover:text-blue-dark" to="/">
+						Home
+					</Link>
+				</div>
+				<div>
+					<Link className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2" to="/items">
+						News
+					</Link>
+					<Link className="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2" to="/crud">
+						CrudApp
+					</Link>
+				</div>
+			</nav>
 
-			{loading ? (
-				<div className="font-bold">Loading results...</div>
-			) : (
-				<ul className="">
-					{results.map((result) => (
-						<li key={result.objectID}>
-							<a href={result.url} className="no-underline hover:underline">
-								<b>{" > "}</b> {result.title}
-							</a>
-						</li>
-					))}
-				</ul>
-			)}
-
-			{error && <div className="text-red font-bold">{error.message}</div>}
-		</div>
+			<Routes>
+				<Route exact path="/items" element={<NewsPage />} />
+				<Route exact path="/crud" element={<CrudApp />} />
+				<Route exact path="/" element={<Navigate to="/items" />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
---
+++ src/pages/CrudApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CrudApp(props) {
+	return <div>crud app</div>;
+}
+
+export default CrudApp;
