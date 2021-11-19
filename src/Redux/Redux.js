66a6ed22096a7106c43d671d05c1e285dd21959d
JohnM <johnnymoran44@hotmail.com>
2021-11-19 11:14:05 +0100

    Update Redux.js

--- src/Redux/Redux.js
+++ src/Redux/Redux.js
@@ -1,18 +1,40 @@
 import { createStore } from "redux";
 
+let result = [];
 const initialState = { count: 0 };
 
+//ACTION GENERATORS
+//Use destructuring with default values
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+	type: "INCREMENT",
+	incrementBy,
+});
+
+//Without destructuring and default values
+/* const incrementCount = (payload = {}) => ({
+	type: "INCREMENT",
+	incrementBy: typeof payload.incrementBy === "number" ? payload.incrementBy : 1,
+}); */
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+	type: "DECREMENT",
+	decrementBy,
+});
+
+const resetCount = () => ({
+	type: "RESET",
+});
+
+//Reducer
 const store = createStore((state = initialState, action) => {
 	switch (action.type) {
 		case "INCREMENT":
-			const incrementBy = typeof action["incrementBy"] === "number" ? action["incrementBy"] : 1;
 			return {
-				count: state.count + incrementBy,
+				count: state.count + action.incrementBy,
 			};
 		case "DECREMENT":
-			const decrement = typeof action["decrement"] === "number" ? action["decrement"] : 1;
 			return {
-				count: state.count - decrement,
+				count: state.count - action.decrementBy,
 			};
 
 		case "RESET":
@@ -26,14 +48,23 @@ const store = createStore((state = initialState, action) => {
 
 store.subscribe(() => {
 	console.log(store.getState());
+	result.push(JSON.stringify(store.getState()));
 });
 
 //Actions - is an object that is sent to the store
-store.dispatch({ type: "INCREMENT", incrementBy: 5 });
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "RESET" });
-store.dispatch({ type: "DECREMENT", decrement: 8 });
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(resetCount());
+store.dispatch(decrementCount({ decrementBy: 8 }));
 
 export default function Redux() {
-	return <div className="text-center m-4">redux store</div>;
+	return (
+		<div className="text-center m-4">
+			redux store
+			<div>
+				{result.map((item, i) => (
+					<div key={i}>{item}</div>
+				))}
+			</div>
+		</div>
+	);
 }
